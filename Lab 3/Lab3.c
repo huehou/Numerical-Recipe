0f374c4db2637bec454b739fd267e4f557884456
huehou <Alexander_hue1993@hotmail.com>
2018-10-22 22:50:59 +0800

    Calculated initial energy value

--- Lab 3/Lab3.c
+++ Lab 3/Lab3.c
@@ -17,6 +17,9 @@ input: - size: the number of rows and columns of A and B parallelogram, which we
 void MonteCarlo(int size)
 {
     double A[size*size + 1], B[size*size + 1];
+    double J = 1.;
+    double energy = 0.;
+
     // Setup the initial grid
     for(int i = 1; i <= size*size; i++)
     {
@@ -24,10 +27,46 @@ void MonteCarlo(int size)
         B[i] = drand64() > 0.5 ? 1. : -1;
     }
 
+    // Calculate initial energy
+    for(int i = 1; i <= size*size; i++)
+    {
+        if(i == size)
+        {
+            energy += -J*A[i]*B[i];
+            energy += -J*A[1]*B[i];
+            energy += -J*A[size*size - size + 1]*B[i];
+        }
+        else if(i < size)
+        {
+            energy += -J*A[i]*B[i];
+            energy += -J*A[i+1]*B[i];
+            energy += -J*A[size*size-size+1+i]*B[i];
+        }
+        else if(i == size*size)
+        {
+            energy += -J*A[i]*B[i];
+            energy += -J*A[size*size-size+1]*B[i];
+            energy += -J*A[size*size-2*size+1]*B[i];
+        }
+        else if (i%size == 0)
+        {
+            energy += -J*A[i]*B[i];
+            energy += -J*A[i-size+1];
+            energy += -J*A[i-2*size+1];
+        }
+        else
+        {
+            energy += -J*A[i]*B[i];
+            energy += -J*A[i+1]*B[i];
+            energy += -J*A[i-size+1]*B[i];
+        }
+    }
+
     for(int i = 1; i <= size*size; i++)
     {
         printf("%f\t%f\n", A[i], B[i]);
     }
+    printf("Energy is: %f\n", energy);
 }
 
 int main()
